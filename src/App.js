1c6642abb650bd2963e4ab7ecc2a158fa4c996d2
hariharan <36292275+fabhari@users.noreply.github.com>
2023-09-30 21:02:50 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="outer">
+      <div className="progress">
+        <div className="left">
+          <div>Start</div>
+          <div className="current"></div>
+        </div>
+        <div className="right">
+          <div>Ament</div>
+          <div className="current">Consectetur</div>
+        </div>
+      </div>
+    </div>
+  );
+}
